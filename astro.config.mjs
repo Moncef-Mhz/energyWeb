2012ec50360b20f4155d05e4887d97cf28eb16b7
moncef <meharzimoncef57@gmail.com>
2025-10-31 11:10:07 +0100

    feat: add header, hero, and details components; integrate Tailwind CSS and React support
    - Updated package.json to include necessary dependencies for React and Tailwind CSS.
    - Created a new header component for navigation.
    - Developed a hero component showcasing key information and call-to-action buttons.
    - Added a details component to display company logos and descriptions.
    - Introduced global CSS styles with Tailwind CSS integration.
    - Configured TypeScript to support React JSX syntax.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Plus Jakarta Sans",
+        cssVariable: "--font-jakarta",
+      },
+    ],
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
